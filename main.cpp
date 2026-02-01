5dc511e438d86689ff952c27444b2b4ddac50d1f
CrepHidery <ryan110413@outlook.com>
2026-02-01 09:48:00 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include "main_console.cpp"
 #include <gtk/gtk.h>
 int main(int argc, char** argv) {
@@ -9,6 +10,22 @@ int main(int argc, char** argv) {
     else {
         DEG_LOG(I, "Starting GUI mode...");
         gtk_init(&argc, &argv);
+        GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+        gtk_window_set_title(GTK_WINDOW(window), "SFD Tool GUI");
+        gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+        
+        // 设置关闭信号
+        g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+        
+        // 这里添加你的GUI界面代码
+        GtkWidget *label = gtk_label_new("SFD Tool GUI Mode");
+        gtk_container_add(GTK_CONTAINER(window), label);
+        
+        // 显示所有组件
+        gtk_widget_show_all(window);
+        
+        // 启动GTK主循环
+        gtk_main();
         return 0;
     }
 }
