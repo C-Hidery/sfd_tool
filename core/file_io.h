05a67a0ab70234e74aa318d5f2da62d295b30a3b
CrepHidery <ryan110413@outlook.com>
2026-05-23 10:39:41 +0800

    feat: RAII式(my_)oxfopen

--- core/file_io.h
+++ core/file_io.h
@@ -1,8 +1,14 @@
 #pragma once
 #include <stdio.h>
 
-FILE* xfopen(const char* fn, const char* mode);
-FILE* my_fopen(const char *fn, const char *mode);
-FILE* my_xfopen(const char* fn, const char* mode);
+struct FileDeleter {
+    void operator()(FILE* f) const noexcept {
+        if (f) fclose(f);
+    }
+};
+using UniqueFile = std::unique_ptr<FILE, FileDeleter>;
+
 FILE* oxfopen(const char* fn, const char* mode);
 FILE* my_oxfopen(const char* fn, const char* mode);
+UniqueFile oxfopen_unique(const char* fn, const char* mode);
+UniqueFile my_oxfopen_unique(const char* fn, const char* mode);
