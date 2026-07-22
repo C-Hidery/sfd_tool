0fff18e57cc16a404fc4235ecc08f6050443222e
CrepHidery <ryan110413@outlook.com>
2026-07-22 14:07:15 +0800

    fix: Wrapper

--- Wrapper.h
+++ Wrapper.h
@@ -7,24 +7,40 @@
 #pragma once
 
 #include <windows.h>
+#include <winsock2.h>
+#include <mutex>
 
+// ---------- 结构体定义（包含 Socket 代理所需成员） ----------
 typedef struct {
-	void *obj;
+    SOCKET sock;           // Socket 连接句柄
+    std::mutex mtx;        // 线程安全锁
 } ClassHandle;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// ---------- Wrapper API ----------
 ClassHandle *createClass();
 void destroyClass(ClassHandle *handle);
+
 BOOL call_Initialize(ClassHandle *handle);
 void call_Uninitialize(ClassHandle *handle);
+
 int call_Read(ClassHandle *handle, UCHAR *m_RecvData, int max_len, int dwTimeout);
 int call_Write(ClassHandle *handle, UCHAR *lpData, int iDataSize);
+
 BOOL call_ConnectChannel(ClassHandle *handle, DWORD dwPort, ULONG ulMsgId, DWORD Receiver);
 BOOL call_DisconnectChannel(ClassHandle *handle);
+
 BOOL call_GetProperty(ClassHandle *handle, LONG lFlags, DWORD dwPropertyID, LPVOID pValue);
 BOOL call_SetProperty(ClassHandle *handle, LONG lFlags, DWORD dwPropertyID, LPCVOID pValue);
+
 void call_Clear(ClassHandle *handle);
 void call_FreeMem(ClassHandle *handle, LPVOID pMemBlock);
 
-
-
+#ifdef __cplusplus
+}
 #endif
+
+#endif // WRAPPER_H
