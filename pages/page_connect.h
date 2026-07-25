a7a8b584cb828f1a2dfcda256f423eee2795c8b3
CrepHidery <ryan110413@outlook.com>
2026-07-25 19:20:21 +0800

    fix: Window size; chsize function update

--- pages/page_connect.h
+++ pages/page_connect.h
@@ -4,6 +4,7 @@
  */
 #pragma once
 #include "ui/GtkWidgetHelper.hpp"
+#include "../core/config_service.h"
 
 // 创建 Connect 标签页 UI 并添加到 notebook
 // 返回 connectPage 中创建的状态标签用于底部控制栏
@@ -11,3 +12,5 @@ GtkWidget* create_connect_page(GtkWidgetHelper& helper, GtkWidget* notebook);
 
 // 绑定 Connect 页面信号
 void bind_connect_signals(GtkWidgetHelper& helper, int argc, char** argv);
+
+sfd::ConfigService* ensure_config_service();
